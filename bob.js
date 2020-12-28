2e22d96244266847055d3f6bdd97da29534c3381
Prince-USA <69021735+Prince-USA@users.noreply.github.com>
2020-12-27 19:50:01 -0800

    Add files via upload

---
+++ bob.js
@@ -0,0 +1,28 @@
+class Bob {
+
+    constructor(x, y) {
+        var options = {
+            'restitution': 0.3,      
+        };
+        this.radius = 35;
+        this.body = Bodies.circle(x, y, this.radius / 2, options);
+
+
+        World.add(world, this.body);
+    }
+
+    display() {
+        var pos = this.body.position;
+        var angle = this.body.angle;
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        rectMode(CENTER);
+        fill("red");
+        stroke("red")
+        strokeWeight(4)
+        ellipse(0, 0, this.radius, this.radius);
+        pop();
+
+    }
+};
